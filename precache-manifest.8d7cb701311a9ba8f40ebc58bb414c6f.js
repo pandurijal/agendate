b1fd014b4510158560c63f53bb52bbb109042c94
Pandu Rijal Pasa <pandurijal96@gmail.com>
2019-01-29 23:23:37 +0700

    Updates

---
+++ precache-manifest.8d7cb701311a9ba8f40ebc58bb414c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ae5b5886780f4bff02",
+    "url": "/agendate/static/js/runtime~main.31ae5b58.js"
+  },
+  {
+    "revision": "fe4e96af3e0dd2edf169",
+    "url": "/agendate/static/js/main.fe4e96af.chunk.js"
+  },
+  {
+    "revision": "38db06a7c462ae878582",
+    "url": "/agendate/static/js/1.38db06a7.chunk.js"
+  },
+  {
+    "revision": "fe4e96af3e0dd2edf169",
+    "url": "/agendate/static/css/main.4224a435.chunk.css"
+  },
+  {
+    "revision": "38db06a7c462ae878582",
+    "url": "/agendate/static/css/1.6b9d78e0.chunk.css"
+  },
+  {
+    "revision": "b0e3d14c6e74b4b52189211b429a32de",
+    "url": "/agendate/index.html"
+  }
+];
